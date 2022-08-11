6823b1b70e3fefd3018905735cc4c36f51ba421e
Sean Phaphui <sean.phaphui@navy.mil>
2022-08-11 14:29:46 -0700

    Change trial

--- libraries/kraven/steps/trial.groovy
+++ libraries/kraven/steps/trial.groovy
@@ -1,5 +1,5 @@
 void call(){
     stage("Build: Maven"){
-        println "trial from the kraven library ${config.BUILD_NUMBER}"
+        println "trial from the kraven library ${config.BUILD_NUMBER_ENV}"
     }
 }
