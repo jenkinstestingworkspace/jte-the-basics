5cf292244a4e7177971409d73559edd086c5ec80
Sean Phaphui <sean.phaphui@navy.mil>
2022-08-11 15:27:43 -0700

    Update trial

--- libraries/kraven/steps/trial.groovy
+++ libraries/kraven/steps/trial.groovy
@@ -1,6 +1,7 @@
 void call(){
     stage("Build: Maven"){
-        println "Build Number ${config.BUILD_NUMBER_ENV}"
-        println "Prefix Release Version ${config.RELEASE_VERSION_SERIAL_NUMBER_PREFIX}"
+        script {
+            "echo ${config.BUILD_NUMBER_ENV}"
+        }
     }
 }
