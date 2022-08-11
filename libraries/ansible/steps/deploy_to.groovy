c4c6655b0872d6a9beda8f6ae98fd0482aa05c98
Sean Phaphui <sean.phaphui@navy.mil>
2022-08-11 11:09:25 -0700

    Change deploy

--- libraries/ansible/steps/deploy_to.groovy
+++ libraries/ansible/steps/deploy_to.groovy
@@ -4,10 +4,5 @@ void call(app_env){
         app_env.ip_addresses.each{ ip ->
             println "deploying to ${ip}"
         }
-        if(config.requiresApproval){
-            timeout(time: 1, unit: 'MINUTES') {
-                input 'Approve the deployment?'
-            }
-        }
     }
 }
